d53f406a06843c98ecf640c7744bb7236d36cfd1
Gerhardus <hardus.cronje@gmail.com>
2026-04-12 23:21:20 +0200

    feat: full navigation is now in effect

--- inverter/inverter.pl
+++ inverter/inverter.pl
@@ -1,24 +1,123 @@
 #!/bin/perl
+
 use XML::Twig;
 
-my ($device_id_file, $controller_mapping_file) = @ARGV;
+# /roms/tools/inverter
+my ($device_id_file, $workdir) = @ARGV;
 
 unless (defined $device_id_file) {
 	$device_id_file = "/etc/emulationstation/es_input.cfg";
 }
 
-unless (defined $controller_mapping_file) {
-	# ./tools/PortMaster/batocera/gamecontrollerdb.txt
-	# ./tools/PortMaster/knulli/gamecontrollerdb.txt
-	# ./tools/PortMaster/gamecontrollerdb.txt
-	$controller_mapping_file = "/roms/tools/PortMaster/gamecontrollerdb.txt";
+unless (defined $workdir) {
+	$workdir = "/roms/tools/inverter"
+}
+
+main($device_id_file, $workdir);
+
+sub main {
+	my ($device_id_file, $workdir) = @_;
+	my $controller_file_list_file_name = "controller_file_list.txt";
+	my $backup_file = "$workdir/$controller_file_list_file_name";
+
+	unless (-e $backup_file) {
+		refresh_backup_file($backup_file);
+	}
+
+	# Check existing controller files
+	while(my $choice = draw_main_menu()) {
+		if($choice eq "a"){
+			my $controller_mapping_file = draw_select_controller_file($backup_file);
+			next unless($controller_mapping_file);
+			chomp $controller_mapping_file;
+			handle_txt_input_file($device_id_file, $controller_mapping_file);
+		} elsif($choice eq "b") {
+			handle_xml_input_file($device_id_file);
+		} elsif($choice eq "c") {
+			refresh_backup_file($backup_file);
+		};
+	}
+}
+
+sub refresh_backup_file {
+	my ($backup_file) = @_;
+	system("find / -name gamecontrollerdb.txt 2>/dev/null | tee $backup_file | dialog --backtitle \"Analog Stick Inverter by Gerhardus\" --progressbox 0 0");
+	open(my $fh, "<", $backup_file);
+	my @files = <$fh>;
+	close($fh);
+}
+
+sub draw_select_controller_file {
+	my ($backup_file) = @_;
+
+	open(my $fh, "<", $backup_file) or system("dialog --backtitle \"Analog Stick Inverter by Gerhardus\" --msgbox \"Something went wrong\"");
+	my @files = <$fh>;
+	close($fh);
+	my $length = @files;
+
+	my $temp_file_name = "/tmp/tempcontrollerfileselection";
+	my $description = "Select a file";
+
+	my %files;
+	my $char = 'a';
+	my $command = "dialog " .
+		"--backtitle \"Analog Stick Inverter by Gerhardus\" " .
+		"--cancel-label BACK " .
+		"--menu \"$description\" 0 0 $length ";
+
+	foreach $file (@files) {
+		$files{$char} = $file;
+		$command .= "$char ";
+		$command .= "\"$file\" ";
+		$char++;
+	}
+	$command .= "2> $temp_file_name";
+
+	system($command);
+	open(my $tfh, "<", $temp_file_name) or return undef;
+	my $choice = <$tfh>;
+	close($tfh);
+	unlink($temp_file_name);
+
+	return $files{$choice};
 }
 
 
-handle_txt_input_file($device_id_file, $controller_mapping_file);
+# MENU 1: Options:
+# 1. Emulationstation (only /etc/emulationstation/es_input.cfg)
+# 2. Other
+# 3. Scan for controls files (refresh)
+sub draw_main_menu {
+	my $tempfilename = "/tmp/tempmainmenuchoice";
+	my $description = "Choose which analog sticks to invert.\
+Emulationstation is the main menu, while
+Games is related to games.
+Emulationstation -> es_input.cfg
+Games            -> gamecontrollerdb.txt";
 
-# TODO: Add main menu to choose action
-# handle_xml_input_file($device_id_file);
+	system("dialog " . 
+		"--backtitle \"Analog Stick Inverter by Gerhardus\" " .
+		# "--default-item $last_choice " .
+		"--title \"Analog Stick Inverter\" " .
+		"--cancel-label EXIT " .
+		"--no-collapse " .
+		"--clear " .
+		"--menu \"$description\" " .
+		# WIDTH HEIGHT NUM OPTIONS
+		"0 0 3 " .
+		# OPTIONS
+		"a \"PLATFORMS ETC\" " .
+		"b \"EMULATIONSTATION\" " .
+		"c \"REFRESH GAMES LIST\" " .
+		"2> $tempfilename"
+	);
+
+	my $temp_file = open(my $fh, "<", $tempfilename) or return undef;
+	my $choice = <$fh>;
+	close($fh);
+	unlink($temp_file);
+	return $choice;
+}
 
 sub handle_txt_input_file {
 	my ($device_id_file, $controller_mapping_file) = @_;
@@ -39,7 +138,7 @@ sub handle_txt_input_file {
 	my @relevant_keys = grep(/(:?left|right)(?:x|y|stick)/, @order);
 
 	my $last_choice = "a";
-	while($item = draw_controller_menu(\@relevant_keys, \%hotkeys, $last_choice, "TEXT")) {
+	while($item = draw_controller_menu(\@relevant_keys, \%hotkeys, $last_choice, "GAME")) {
 		$last_choice = $item;
 		# "a \"BOTH:    SWAP\" " .
 		if ($item eq "a") {
@@ -84,11 +183,14 @@ sub handle_txt_input_file {
 
 			close($cm_file);
 			close($temp_file);
-			rename("$controller_mapping_file.tmp", $controller_mapping_file);
-			system("dialog --title \"FINISHED\" " .
-				"--msgbox " .
-				"\"Sucessfully updated config file\" " .
-				"10 30");
+			unless (-z "$controller_mapping_file.tmp") {
+				rename("$controller_mapping_file.tmp", $controller_mapping_file);
+				system("dialog --title \"FINISHED\" " .
+					"--backtitle \"Analog Stick Inverter by Gerhardus\" " .
+					"--msgbox " .
+					"\"Sucessfully updated config file\" " .
+					"0 0");
+			}
 			last;
 		}
 	};
@@ -137,7 +239,7 @@ sub invert_key {
 # DATA SOURCES
 sub get_device_id {
 	my ($input_path) = @_;
-	open(my $fh, "<", $input_path) or die "$!";
+	open(my $fh, "<", $input_path) or return undef;
 	my $device_id;
 
 	while (my $line = <$fh>) {
@@ -207,7 +309,7 @@ sub draw_controller_menu {
 	my @relevant_keys = @$relevant_keys;
 
 	my $current_bindings = "";
-	if ($type eq "TEXT") {
+	if ($type eq "GAME") {
 		$current_bindings = print_hotkeys(\@relevant_keys, \%hotkeys);
 	} else {
 		$current_bindings = print_xml_hotkeys(\@relevant_keys, \%hotkeys);
@@ -220,7 +322,8 @@ sub draw_controller_menu {
 	system("dialog " . 
 		"--backtitle \"Analog Stick Inverter by Gerhardus\" " .
 		"--default-item $last_choice " .
-		"--title \"Analog Stick Inverter\" " .
+		"--cancel-label BACK " .
+		"--title \"Invert analog sticks for $type\" " .
 		"--no-collapse " .
 		"--clear " .
 		"--menu \"$current_bindings\" " .
@@ -267,7 +370,7 @@ sub handle_xml_input_file {
 
 	my $last_choice = "a";
 
-	while($item = draw_controller_menu(\@relevant_keys, \%hotkeys, $last_choice, "XML")) {
+	while($item = draw_controller_menu(\@relevant_keys, \%hotkeys, $last_choice, "SYSTEM")) {
 		$last_choice = $item;
 		# "a \"BOTH:    SWAP\" " .
 		if ($item eq "a") {
@@ -323,12 +426,16 @@ sub handle_xml_input_file {
 			open(my $fh, ">", "$file_name.tmp") or die "$!";
 			$twig->flush($fh);
 			close($fh);
-			rename("$file_name.tmp", $file_name);
 
-			system("dialog --title \"FINISHED\" " .
-				"--msgbox " .
-				"\"Sucessfully updated config file\" " .
-				"10 30");
+			unless (-z "$file_name.tmp") {
+				rename("$file_name.tmp", $file_name);
+				system("dialog --title \"FINISHED\" " .
+					"--backtitle \"Analog Stick Inverter by Gerhardus\" " .
+					"--msgbox " .
+					"\"Sucessfully updated config file\" " .
+					"10 30");
+			}
+
 			last;
 		}
 	};
@@ -431,3 +538,4 @@ sub to_constant_length_string {
 
     return $str . (" " x ($len - length($str)));
 }
+
